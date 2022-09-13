fa94e552e1312fa87900707ed2e8d4e93863c8c2
GaddameediSaipriya <113419037+GaddameediSaipriya@users.noreply.github.com>
2022-09-13 22:51:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+%%writefile app.py
+import streamlit as st
+import joblib
+model = joblib.load('virat')
+st.title('Virat Centuries')
+ip = st.text_input('Enter the out/Notout')
+op = model.predict([ip])
+if st.button('predict'):
+  st.title(op[0])
